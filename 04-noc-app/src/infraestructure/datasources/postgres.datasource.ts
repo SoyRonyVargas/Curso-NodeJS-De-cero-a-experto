a438620a56c663777f624c7f405eb8ba65d4dd59
Rony Vargas <47135307+SoyRonyVargas@users.noreply.github.com>
2024-05-17 12:57:12 -0500

    feat: datasource prisma

---
+++ 04-noc-app/src/infraestructure/datasources/postgres.datasource.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, SeveriityLevel } from "@prisma/client";
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogSeverityLevel, LogEntity } from "../../domain/entity/log.entity";
+
+const prisma = new PrismaClient()
+
+export class PostgresLogDataSource implements LogDataSource {
+    
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        
+        const logs = await prisma.logModel.findMany({
+            where: {
+                level: severityLevel
+            }
+        })
+
+        return logs
+
+    }
+    
+    async saveLog(log: LogEntity): Promise<void> {
+        
+        await prisma.logModel.create({
+            data: {
+                ...log,
+                level: log.level as SeveriityLevel,
+            }
+        })
+
+        return;
+
+    }
+}
